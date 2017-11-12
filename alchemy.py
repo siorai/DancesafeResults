1202c1f87325b7fa6390e4e13e029b316ba9901e
Paul Waldorf <siorai@gmail.com>
2017-11-12 08:36:57 -0500

    Cleaned up a bit, prepping for some db changes

--- alchemy.py
+++ alchemy.py
@@ -3,10 +3,9 @@ from sqlalchemy import create_engine, Boolean, Column, Integer, String, Text, \
     ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.sql import func, text
 from sqlalchemy.dialects.postgresql import UUID
 
-from wtforms import RadioField, SelectField, DateTimeField, IntegerField, Form, BooleanField, StringField, PasswordField, validators
+from wtforms import IntegerField, Form, StringField, validators
 
 
 from secrets import pgSecret
@@ -22,8 +21,10 @@ session = Session()
 
 class User(Base):
     __tablename__ = 'users'
-    
-    id = Column(UUID(as_uuid=True), primary_key=True, server_default=sqlalchemy.text("uuid_generate_v4()"))
+
+    id = Column(UUID(as_uuid=True),
+                primary_key=True,
+                server_default=sqlalchemy.text("uuid_generate_v4()"))
     username = Column(String(30), unique=True, nullable=False)
     fullname = Column(String(100), unique=True, nullable=False)
     email = Column(String(50), unique=True, nullable=False)
@@ -37,24 +38,27 @@ class NewUser(Form):
     username = StringField('Username', [validators.Length(min=4, max=25)])
     chapter = StringField('Chapter', [validators.Length(min=4, max=40)])
 
+
 class EventInfo(Base):
     __tablename__ = 'eventinfo'
 
-    id = Column(UUID(as_uuid=True), primary_key=True, server_default=sqlalchemy.text("uuid_generate_v4()"))
+    id = Column(UUID(as_uuid=True),
+                primary_key=True,
+                server_default=sqlalchemy.text("uuid_generate_v4()"))
     name = Column(String(50), nullable=False)
     year = Column(Integer, nullable=False)
     city = Column(String(50), nullable=False)
     state = Column(String(2), nullable=False)
     region = Column(String(25))
-    
+
+
 class EventForm(Form):
     name = StringField('Name', [validators.Length(min=4, max=50)])
     year = IntegerField('Year', [validators.Length(min=4, max=4)])
     city = StringField('City', [validators.Length(min=2, max=50)])
-    state = StringField('State', [validators.Length(min=2, max=2, message="Please use abbrevition")])
+    state = StringField('State', [validators.Length(min=2, max=2)])
     region = StringField('Region', [validators.Length(min=2, max=25)])
-    #datestart = DateTimeField('datestart')
-    #dateend = DateTimeField('dateend')
+
 
 class Survey(Base):
     __tablename__ = 'survey'
@@ -69,20 +73,23 @@ class Survey(Base):
                 self.questionid,
                 self.answer)
 
+
 class Questions(Base):
     __tablename__ = 'questions'
-    
+
     id = Column(Integer, primary_key=True)
     detail = Column(Text)
+
 
 class NewQuestion(Form):
     detail = StringField('detail', [validators.Length(min=4, max=500)])
 
+
 class Sample(Base):
     __tablename__ = 'sample'
-    
+
     id = Column(Integer, primary_key=True)
-    #make event id back populate from EventInfo Table...
+    # make event id back populate from EventInfo Table...
     eventid = Column(UUID, ForeignKey('eventinfo.id'))
     event = relationship('EventInfo', backref='eventinfo')
     surveyid = Column(Integer, ForeignKey('survey.id'))
@@ -90,7 +97,7 @@ class Sample(Base):
     expectedsubstanceid = Column(Integer, ForeignKey('substances.id'))
     groundscorebool = Column(Boolean)
 
-    #same with the User table....
+    # same with the User table....
     shiftlead = Column(UUID, ForeignKey('users.id'))
     tester = Column(UUID, ForeignKey('users.id'))
     recorder = Column(UUID, ForeignKey('users.id'))
@@ -98,16 +105,20 @@ class Sample(Base):
     result = Column(Integer, ForeignKey('substances.id'))
     commentbool = Column(Boolean)
 
+
 class SampleTest(Base):
     __tablename__ = 'sampletest'
 
-    id = Column(UUID(as_uuid=True), primary_key=True, server_default=sqlalchemy.text("gen_random_uuid()"))
+    id = Column(UUID(as_uuid=True),
+                primary_key=True,
+                server_default=sqlalchemy.text("gen_random_uuid()"))
     chapterName = Column(String)
     eventid = Column(UUID(as_uuid=True), ForeignKey('eventinfo.id'))
     shiftLead = Column(UUID(as_uuid=True), ForeignKey('users.id'))
     tester = Column(UUID(as_uuid=True), ForeignKey('users.id'))
     recorder = Column(UUID(as_uuid=True), ForeignKey('users.id'))
     ts = Column(DateTime, server_default=sqlalchemy.text("now()"))
+
 
 class SampleTestForm(Form):
     chapterName = StringField('chapterName')
@@ -116,23 +127,28 @@ class SampleTestForm(Form):
     tester = StringField('tester')
     recorder = StringField('recorder')
 
+
 class Substances(Base):
     __tablename__ = 'substances'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    
+
+
 class Tests(Base):
     __tablename__ = 'tests'
-    
+
     id = Column(Integer, primary_key=True)
     reagentid = Column(Integer, ForeignKey('reagent.id'))
-    colorrank = Column(Integer) 
+    colorrank = Column(Integer)
+
 
 class TestResults(Base):
     __tablename__ = 'testresults'
 
-    id = Column(UUID(as_uuid=True), primary_key=True, server_default=sqlalchemy.text("gen_random_uuid()"))
+    id = Column(UUID(as_uuid=True),
+                primary_key=True,
+                server_default=sqlalchemy.text("gen_random_uuid()"))
     sampleid = Column(Integer, ForeignKey('sample.id'))
     testid = Column(String)
     reactioncolor = Column(String)
@@ -140,19 +156,14 @@ class TestResults(Base):
 
 class ExpectedReactions(Base):
     __tablename__ = 'expectedreactions'
-    
+
     id = Column(Integer, primary_key=True)
     testid = Column(Integer, ForeignKey('tests.id'))
     expectedcolor = Column(String)
+
 
 class Reagent(Base):
     __tablename__ = 'reagent'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
-
-class NewUser(Form):
-    name = StringField('Name', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    chapter = StringField('Chapter', [validators.Length(min=4, max=40)])
