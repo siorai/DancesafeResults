e7df86ee8e54e9c03481676724f6fac2cd78ce87
Paul Waldorf <siorai@gmail.com>
2018-01-05 06:15:44 -0500

    More radically different changes to the ovreall database schema
    With this, I should be satisfied for a bit and see no reason to
    make any additional changes.
    
    Please note, that this commit -only- changes SQLAlchemy's declarative
    classes that both create the tables in the database itself, and,
    provide those tables as class objects to be used in the rest of the
    app.
    
    Everything else, as of right this second, should be broken. Will
    start on reformulating individual pages and probably massively
    overhauling them too, in the next 12 hours or so.

--- alchemy.py
+++ alchemy.py
@@ -2,11 +2,8 @@ import sqlalchemy
 from sqlalchemy import create_engine, Boolean, Column, Integer, String, Text, \
     ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects.postgresql import UUID
-
-from wtforms import IntegerField, Form, StringField, validators
-
 
 from secrets import pgSecret
 
@@ -21,17 +18,71 @@ session = Session()
 # Tables
 
 
-class User(Base):
-    __tablename__ = 'users'
+class Colors(Base):
+    __tablename__ = 'colors'
 
     id = Column(UUID(as_uuid=True),
                 primary_key=True,
-                server_default=sqlalchemy.text("uuid_generate_v4()"))
-    username = Column(String(30), unique=True, nullable=False)
-    fullname = Column(String(100), unique=True, nullable=False)
-    email = Column(String(50), unique=True, nullable=False)
-    facebookurl = Column(String(50), unique=True, nullable=True)
-    chapter = Column(String(50), nullable=True)
+                server_default=sqlalchemy.text("gen_random_uuid()"))
+    name = Column(String, nullable=False)
+    hex = Column(Integer, nullable=True)
+
+
+class Events(Base):
+    __tablename__ = 'event'
+
+    id = Column(UUID(as_uuid=True),
+                primary_key=True,
+                server_default=sqlalchemy.text("gen_random_uuid()"))
+    name = Column(String(50), nullable=False)
+    year = Column(Integer, nullable=False)
+    city = Column(String(50), nullable=False)
+    state = Column(String(2), nullable=False)
+    region = Column(String(25))
+    author = Column(UUID(as_uuid=True),
+                    ForeignKey('users.id'),
+                    nullable=False)
+    ts = Column(DateTime, server_default=sqlalchemy.text("now()"))
+
+
+class ExpectedReactions(Base):
+    __tablename__ = 'expectedreactions'
+
+    substanceid = Column(UUID(as_uuid=True),
+                         ForeignKey('substances.id'),
+                         primary_key=True,
+                         nullable=False)
+    reactionid = Column(UUID(as_uuid=True),
+                        ForeignKey('reactions.id'),
+                        primary_key=True,
+                        nullable=False)
+    colorid = Column(UUID(as_uuid=True),
+                     ForeignKey('colors.id'),
+                     primary_key=True,
+                     nullable=False)
+
+
+class MaterialType(Base):
+    __tablename__ = 'materialtype'
+
+    id = Column(UUID(as_uuid=True),
+                primary_key=True,
+                server_default=sqlalchemy.text("gen_random_uuid()"))
+    name = Column(String)
+    description = Column(Text)
+
+
+class ReagentColorList(Base):
+    __tablename__ = 'reagentcolorlist'
+
+    reaction = Column(UUID(as_uuid=True),
+                      ForeignKey('reactions.id'),
+                      primary_key=True,
+                      nullable=False)
+    color = Column(UUID(as_uuid=True),
+                   ForeignKey('colors.id'),
+                   primary_key=True,
+                   nullable=False)
 
 
 class Reactions(Base):
@@ -39,55 +90,67 @@ class Reactions(Base):
 
     id = Column(UUID(as_uuid=True),
                 primary_key=True,
-                server_default=sqlalchemy.text("uuid_generate_v4()"))
-    reagent = Column(UUID(as_uuid=True))
-    reactionint = Column(Integer(2), unique=False, nullable=False)
+                server_default=sqlalchemy.text("gen_random_uuid()"))
+    reagentid = Column(UUID(as_uuid=True),
+                       ForeignKey('reagent.id'),
+                       nullable=False)
+    reactionint = Column(Integer, unique=False, nullable=False)
 
 
-class Colors(Base):
-    __tablename__ = 'colors'
+class Reagents(Base):
+    __tablename__ = 'reagent'
 
     id = Column(UUID(as_uuid=True),
                 primary_key=True,
-                server_default=sqlalchemy.text("uuid_generate_v4()"))
+                server_default=sqlalchemy.text("gen_random_uuid()"))
     name = Column(String)
-    color = Column(Integer)
+    author = Column(UUID(as_uuid=True),
+                    ForeignKey('users.id'),
+                    nullable=False)
+    datecreated = Column(DateTime)
+    description = Column(Text)
 
 
-# in progress, needs back references
-class ReagentColorList(Base):
-    __tablename__ = 'colors'
-
-    reaction = Column(UUID(as_uuid=True))
-    color = Column(UUID(as_uuid=True))
-
-
-class NewUser(Form):
-    name = StringField('Name', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    chapter = StringField('Chapter', [validators.Length(min=4, max=40)])
-
-
-class EventInfo(Base):
-    __tablename__ = 'eventinfo'
+class Sample(Base):
+    __tablename__ = 'sample'
 
     id = Column(UUID(as_uuid=True),
                 primary_key=True,
-                server_default=sqlalchemy.text("uuid_generate_v4()"))
-    name = Column(String(50), nullable=False)
-    year = Column(Integer, nullable=False)
-    city = Column(String(50), nullable=False)
-    state = Column(String(2), nullable=False)
-    region = Column(String(25))
+                server_default=sqlalchemy.text("gen_random_uuid()"))
+    eventid = Column(UUID(as_uuid=True),
+                     ForeignKey('event.id'),
+                     nullable=False)
+    shiftlead = Column(UUID(as_uuid=True),
+                       ForeignKey('users.id'),
+                       nullable=False)
+    tester = Column(UUID(as_uuid=True),
+                    ForeignKey('users.id'),
+                    nullable=False)
+    recorder = Column(UUID(as_uuid=True),
+                      ForeignKey('users.id'),
+                      nullable=False)
+    ts = Column(DateTime, server_default=sqlalchemy.text("now()"))
+    typeid = Column(UUID(as_uuid=True),
+                    ForeignKey('materialtype.id'),
+                    nullable=False)
+    initialsuspect = Column(UUID(as_uuid=True),
+                            ForeignKey('substances.id'),
+                            nullable=False)
+    description = Column(Text)
+    groundscore = Column(Boolean)
+    conclusiveresult = Column(Boolean)
+    finalconclusion = Column(UUID(as_uuid=True),
+                             ForeignKey('substances.id'),
+                             nullable=False)
 
 
-class EventForm(Form):
-    name = StringField('Name', [validators.Length(min=4, max=50)])
-    year = IntegerField('Year', [validators.Length(min=4, max=4)])
-    city = StringField('City', [validators.Length(min=2, max=50)])
-    state = StringField('State', [validators.Length(min=2, max=2)])
-    region = StringField('Region', [validators.Length(min=2, max=25)])
+class Substances(Base):
+    __tablename__ = 'substances'
+
+    id = Column(UUID(as_uuid=True),
+                primary_key=True,
+                server_default=sqlalchemy.text("gen_random_uuid()"))
+    name = Column(String)
 
 
 class Survey(Base):
@@ -97,81 +160,6 @@ class Survey(Base):
     questionid = Column(Integer, ForeignKey('questions.id'))
     answer = Column(Text)
 
-    def __repr__(self):
-        return "<Survey(id='%s', questionid='%s', answer='%s')>" % (
-                self.id,
-                self.questionid,
-                self.answer)
-
-
-class Questions(Base):
-    __tablename__ = 'questions'
-
-    id = Column(Integer, primary_key=True)
-    detail = Column(Text)
-
-
-class NewQuestion(Form):
-    detail = StringField('detail', [validators.Length(min=4, max=500)])
-
-
-class Sample(Base):
-    __tablename__ = 'sample'
-
-    id = Column(Integer, primary_key=True)
-    # make event id back populate from EventInfo Table...
-    eventid = Column(UUID, ForeignKey('eventinfo.id'))
-    event = relationship('EventInfo', backref='eventinfo')
-    surveyid = Column(Integer, ForeignKey('survey.id'))
-    survey = relationship('Survey', backref='survey')
-    expectedsubstanceid = Column(Integer, ForeignKey('substances.id'))
-    groundscorebool = Column(Boolean)
-
-    # same with the User table....
-    shiftlead = Column(UUID, ForeignKey('users.id'))
-    tester = Column(UUID, ForeignKey('users.id'))
-    recorder = Column(UUID, ForeignKey('users.id'))
-    testconclusive = Column(Boolean)
-    result = Column(Integer, ForeignKey('substances.id'))
-    commentbool = Column(Boolean)
-
-
-class SampleTest(Base):
-    __tablename__ = 'sampletest'
-
-    id = Column(UUID(as_uuid=True),
-                primary_key=True,
-                server_default=sqlalchemy.text("gen_random_uuid()"))
-    chapterName = Column(String)
-    eventid = Column(UUID(as_uuid=True), ForeignKey('eventinfo.id'))
-    shiftLead = Column(UUID(as_uuid=True), ForeignKey('users.id'))
-    tester = Column(UUID(as_uuid=True), ForeignKey('users.id'))
-    recorder = Column(UUID(as_uuid=True), ForeignKey('users.id'))
-    ts = Column(DateTime, server_default=sqlalchemy.text("now()"))
-
-
-class SampleTestForm(Form):
-    chapterName = StringField('chapterName')
-    eventName = StringField('eventName')
-    shiftLead = StringField('shiftLead')
-    tester = StringField('tester')
-    recorder = StringField('recorder')
-
-
-class Substances(Base):
-    __tablename__ = 'substances'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-
-
-class Tests(Base):
-    __tablename__ = 'tests'
-
-    id = Column(Integer, primary_key=True)
-    reagentid = Column(Integer, ForeignKey('reagent.id'))
-    colorrank = Column(Integer)
-
 
 class TestResults(Base):
     __tablename__ = 'testresults'
@@ -179,27 +167,36 @@ class TestResults(Base):
     id = Column(UUID(as_uuid=True),
                 primary_key=True,
                 server_default=sqlalchemy.text("gen_random_uuid()"))
-    sampleid = Column(Integer, ForeignKey('sample.id'))
-    testid = Column(UUID(as_uuid=True),
-                    ForeignKey('reactionids.id'))
+    sampleid = Column(UUID(as_uuid=True),
+                      ForeignKey('sample.id'),
+                      nullable=False)
+    reactionid = Column(UUID(as_uuid=True),
+                        ForeignKey('reactions.id'))
     reactioncolor = Column(String)
 
 
-class ExpectedReactions(Base):
-    __tablename__ = 'expectedreactions'
+class Questions(Base):
+    __tablename__ = 'questions'
 
     id = Column(Integer, primary_key=True)
-    testid = Column(Integer, ForeignKey('tests.id'))
-    expectedcolor = Column(String)
+    detail = Column(Text)
+    author = Column(UUID(as_uuid=True),
+                    ForeignKey('users.id'),
+                    nullable=False)
+    ts = Column(DateTime, server_default=sqlalchemy.text("now()"))
 
 
-class Reagent(Base):
-    __tablename__ = 'reagent'
+class Users(Base):
+    __tablename__ = 'users'
 
     id = Column(UUID(as_uuid=True),
                 primary_key=True,
-                server_default=sqlalchemy.text("uuid_generate_v4()"))
-    name = Column(String)
-    author = Column(UUID(as_uuid=True))
-    datecreated = Column(DateTime)
-    description = Column(Text)
+                server_default=sqlalchemy.text("gen_random_uuid()"))
+    username = Column(String(42), unique=True, nullable=False)
+    fullname = Column(String(100), nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
+    facebookurl = Column(String(50), unique=True, nullable=True)
+    instagram = Column(String(50), unique=True, nullable=True)
+    chapter = Column(String(50), nullable=True)
+    password = Column(String, nullable=False)
+    ts = Column(DateTime, server_default=sqlalchemy.text("now()"))
