1896a625fcab429d9bc6d64658dcef5d4c891bb1
Paul Waldorf <paul@dancesafe.org>
2018-04-22 17:56:46 +0000

    More to delete

--- templates/testingsurvey.js
+++
@@ -1,8 +0,0 @@
-define(['react', 'lodash', './testingsurvey.rt'], function (React, _, template) {
-    'use strict';
-
-    return React.createClass({
-        displayName: 'testingsurvey',
-        render: template
-    });
-});
--- testing.c
+++
@@ -1,8 +0,0 @@
-# include <stdio.h>
-
-int main()
-{
-    // printf() displays the string inside the quotation
-    printf("Hello, World!");
-    return 0;
-}
