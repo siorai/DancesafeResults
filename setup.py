b3c52c0a6a809ceabbb0314cef1b92f9ddda3580
Paul Waldorf <paul@dancesafe.org>
2019-02-05 20:28:11 -0800

    Preparing for Black

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Dancesafe',
-    version='',
+    name="Dancesafe",
+    version="",
     packages=find_packages(),
-    url='https://github.com/siorai/DancesafeResults',
-    license='',
-    author='Siorai',
-    author_email='',
-    description='', 
-    install_requires=['flask', 'flask-bcrypt', 'sqlalchemy=1.1.14', 'wtforms', 'flaskbootstrap', 'psycopg2', 'webcolors',
-                      'sqlalchemy']
+    url="https://github.com/siorai/DancesafeResults",
+    license="",
+    author="Siorai",
+    author_email="",
+    description="",
+    install_requires=[
+        "flask",
+        "flask-bcrypt",
+        "sqlalchemy=1.1.14",
+        "wtforms",
+        "flaskbootstrap",
+        "psycopg2",
+        "webcolors",
+        "sqlalchemy",
+    ],
 )
